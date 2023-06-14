972c6d0ecbe0c04ac1a9932de4e29fbb2f0880de
unknown <107675544+tbinhnoob@users.noreply.github.com>
2023-06-14 16:08:30 +0700

    missing dealer AI

--- maingame.py
+++ maingame.py
@@ -10,12 +10,8 @@ for i in number:
 for i in count:
     for j in range(0, 4):
         pts.append(i)
-dealer_card_count=0
-player_card_count=0
 dealer_deck=[]
 player_deck=[]
-dealer_state=-1
-player_state=-1
 total_player_point=0
 total_dealer_point=0
 player_ace_count=0
@@ -25,11 +21,14 @@ stay_count=0
 print("Game start!")
 
 def player_turn():
+    global player_ace_count
+    global total_player_point
     print("Would you like to Draw or Stay? (D/S): ", end='')
     report=input()
     if report=='S': 
         print("You chose stay.")
-        stay_count+=1
+        global stay_count
+        stay_count=1
         return
     else:
         print("You chose draw.")
@@ -43,8 +42,17 @@ def player_turn():
         pts.pop(card)
         msg="You draw "+deck[card]
         print(msg)
+        msg="Your current deck is: "
+        for crd in player_deck:
+            if crd==deck[card]:
+                msg=msg+crd+"."
+            else:
+                msg=msg+crd+", "
+        print(msg)
 
 def dealer_turn():
+    global total_dealer_point
+    global dealer_ace_count
     if total_dealer_point<17:
         print("The dealer chose draw")
         card=random.randint(0, len(deck)-1)
@@ -55,15 +63,67 @@ def dealer_turn():
         else:
             total_dealer_point+=pts[card]
         pts.pop(card)
-        msg="You draw "+deck[card]
-        print(msg)
     else:
         print("The dealer chose stay")
+        global stay_count
         stay_count+=1
         return
 
+def dealer_best_score():
+    global dealer_ace_count
+    global total_dealer_point
+    total_dealer_point+=dealer_ace_count
+    if dealer_ace_count>0:
+        if dealer_ace_count>1 and total_dealer_point+19<=21:
+            total_dealer_point+=19
+        elif dealer_ace_count>1 and total_dealer_point+18<=21:
+            total_dealer_point+=18
+        else:
+            if total_dealer_point+10<=21:
+                total_dealer_point+=10
+            elif total_dealer_point+9<=21:
+                total_dealer_point+=9
+
+def player_best_score():
+    global total_player_point
+    global player_ace_count
+    total_player_point+=player_ace_count
+    if player_ace_count>0:
+        if player_ace_count>1 and total_player_point+19<=21:
+            total_player_point+=19
+        elif player_ace_count>1 and total_player_point+18<=21:
+            total_player_point+=18
+        else:
+            if total_player_point+10<=21:
+                total_player_point+=10
+            elif total_player_point+9<=21:
+                total_player_point+=9
+
+def reveal_result():
+    msg="Your point is: "+str(total_player_point)
+    print(msg)
+    msg="Dealer's point is: "+str(total_dealer_point)
+    print(msg)
+    print("The result is: ", end='')
+    if total_dealer_point>21 and total_player_point>21:
+        print("Draw!")
+    elif total_dealer_point==total_player_point:
+        print("Draw!")
+    elif total_dealer_point>21:
+        print("You win!")
+    elif total_player_point>21:
+        print("Dealer wins!")
+    elif total_dealer_point>total_player_point:
+        print("Dealer wins!")
+    else:
+        print("You win!")
 
 def game_main():
+    global player_ace_count
+    global total_dealer_point
+    global total_player_point
+    global dealer_ace_count
+    global current_turn
     #begin initial phase
     card=random.randint(0, len(deck)-1)
     player_deck.append(deck[card])
@@ -105,7 +165,7 @@ def game_main():
     print("Your current")
 
     #begin draw phase
-    while not (player_state==0 and dealer_state==0):
+    while stay_count<=2:
         msg="It is "+current_turn+"'s turn."
         print(msg)
         if current_turn=="player":
@@ -118,7 +178,9 @@ def game_main():
     #end draw phase
 
     #begin compare phase
-    
+    dealer_best_score()
+    player_best_score()
+    reveal_result()
     #end compare phase
 
 game_main()
